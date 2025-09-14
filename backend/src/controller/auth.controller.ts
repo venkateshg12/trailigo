bf81036fd1a84c22da99a2100e3f288dcf8e475a
venkateshg12 <gvenkatesh1204@gmail.com>
2025-09-14 22:19:36 +0530

    Add logout Handle

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -1,9 +1,11 @@
-import { CREATED } from "../constants/http";
+import { CREATED, OK } from "../constants/http";
+import sessionModel from "../models/session.model";
 import { createAccount } from "../services/auth.services";
 import { verifyOTP } from "../services/verifyOtp.services";
 import catchError from "../utils/catchError"
-import { setAuthCookies } from "../utils/cookies";
-import { registerSchema } from "./auth.schema"
+import { clearAuthCookies, setAuthCookies } from "../utils/cookies";
+import { verifyToken } from "../utils/jwt";
+import { loginSchema, registerSchema } from "./auth.schema"
 
 
 export const registerHandler = catchError(async (req, res) => {
@@ -28,4 +30,24 @@ export const verifyEmail = catchError(async (req, res) => {
     const { userId, code } = req.body;
     const result = await verifyOTP({ userId, code });
     return res.status(200).json(result);
+})
+
+export const loginHandler = catchError(async(req, res) =>{
+    const request = loginSchema.parse({
+        ...req.body,
+        userAgent : req.headers["user-agent"],
+    })
+})
+
+export const logoutHandler = catchError(async (req, res) => {
+    const accessToken = req.cookies.accessToken;
+    const result = verifyToken(accessToken || " ");
+    if ("payload" in result) {
+        const payload = result.payload as any;
+        await sessionModel.findByIdAndDelete(payload.sessionId);
+    }
+    clearAuthCookies(res);
+    return res.status(OK).json({
+        message: "Logout Successful",
+    })
 })
