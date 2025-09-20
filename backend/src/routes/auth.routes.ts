80f32bb56cf390f546c6621e9265662213275b2f
venkateshg12 <gvenkatesh1204@gmail.com>
2025-09-20 21:49:34 +0530

    Add routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { authMiddleWare, loginHandler, logoutHandler, registerHandler, verifyEmail } from "../controller/auth.controller";
+import { getUserInfo, googleAuthCallback, googleAuthInitiate, loginHandler, logoutHandler, registerHandler, verifyEmail } from "../controller/auth.controller";
 
 const authRoutes = Router();
 
@@ -7,7 +7,9 @@ authRoutes.post("/register", registerHandler);
 authRoutes.post("/verify-email", verifyEmail);
 authRoutes.post("/login", loginHandler);
 authRoutes.post("/logout", logoutHandler);
-authRoutes.get("/user", authMiddleWare);
+authRoutes.get("/user", getUserInfo);
+authRoutes.get("/google", googleAuthInitiate);
+authRoutes.get("/google/callback", googleAuthCallback);
 
 
 export default authRoutes;
