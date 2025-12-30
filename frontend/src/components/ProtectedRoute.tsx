ae65231c3fb19d4039f280da9e8581bd22c40267
venkateshg12 <gvenkatesh1204@gmail.com>
2025-12-30 22:45:03 +0530

    fix: resolve main page redirect issue

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -2,8 +2,16 @@ import { useUserInfo } from '@/hooks/useUserInfo';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-  const { data: user } = useUserInfo();
-  return user?.verified ? <Outlet /> : <Navigate to="/" replace />;
+  const { data: user, isLoading } = useUserInfo();
+
+  if (isLoading) {
+    return null;
+  }
+  
+  if (!user || !user.verified) {
+    return <Navigate to="/" replace />;
+  }
+  return <Outlet />
 };
 
 export default ProtectedRoute;
